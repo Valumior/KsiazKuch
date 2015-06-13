6112a88eaebcc2b601aa3b12d7a44c3d4a58392d
Valumior <matinoshadow@gmail.com>
2015-06-13 11:27:42 +0200

    Db script fix. Tables created in DB.

--- DB/DbDef.sql
+++ DB/DbDef.sql
@@ -1,65 +1,44 @@
-drop table skladniki_przepis;
-drop table przepis;
-drop table skladnik;
-drop table wartosci_odzywcze;
-drop table enum_trudnosc;
-drop table enum_miara;
-
-create table enum_trudnosc (
-	id number(11) not null auto increment,
-	nazwa varchar(50),
-	primary key (id)
-);
-
-create table enum_miara (
-	id number(11) auto increment,
-	nazwa varchar(30),
-	primary key (id)
-);
+drop table if exists skladniki_przepis;
+drop table if exists przepis;
+drop table if exists skladnik;
+drop table if exists wartosci_odzywcze;
 
 create table wartosci_odzywcze (
-	id number(11) not null auto increment,
-	kalorie number(5) not null,
-	tluszcze_calkowite number(5) not null,
-	tluszcze_nasycone number(5),
-	tluszcze_nienasycone number(5),
-	cholesterol number(5) not null,
-	sod number(5) not null,
-	weglowodany number(5) not null,
-	blonnik number(5),
-	cukry number(5),
-	bialka number(5) not null,
-	primary key(id)
+	id integer not null primary key autoincrement,
+	kalorie integer not null,
+	tluszcze_calkowite integer not null,
+	tluszcze_nasycone integer,
+	tluszcze_nienasycone integer,
+	cholesterol integer not null,
+	sod integer not null,
+	weglowodany integer not null,
+	blonnik integer,
+	cukry integer,
+	bialka integer not null
 );
 
 create table przepis (
-	id number(11) auto increment primary key,
-	nazwa varchar(200) not null,
-	czas przygotowania number(3) not null,
-	trudnosc number(11),
-	ulubione bit,
-	instrukcja text not null,
-	obrazek varchar(1000),
-	primary key (id),
-	foreign key(trudnosc) references (enum_trudnosc(id))
+	id integer primary key autoincrement,
+	nazwa text not null,
+	czas przygotowania integer not null,
+	trudnosc integer,
+	ulubione numeric,
+	instrukcja text not null
 );
 
 create table skladnik (
-	id number(11) not null auto increment,
-	nazwa varchar(200) not null,
-	miara number(11) not null,
-	wartosci_odzywcze number(11) not null,
-	obrazek varchar(1000),
-	primary key(id),
-	foreign key(miara) references (enum_miara(id)),
-	foreign key(wartosci_odzywcze) references (wartosci_odzywcze(id))
+	id integer not null primary key autoincrement,
+	nazwa text not null,
+	miara integer not null,
+	wartosci_odzywcze integer not null,
+	foreign key(wartosci_odzywcze) references wartosci_odzywcze(id)
 );
 
 create table skladniki_przepis (
-	przepis number(11) not null,
-	skladnik number(11) not null,
-	liczba number(6) not null,
+	przepis integer not null,
+	skladnik integer not null,
+	liczba integer not null,
 	primary key (przepis, skladnik),
-	foreign key(przepis) references (przepis(id)),
-	foreign key(skladnik) references (skladnik(id))
+	foreign key(przepis) references przepis(id),
+	foreign key(skladnik) references skladnik(id)
 );
